0c43366943dd6ae1636d78b02f569663b60cb39f
David Goligorsky <d.goligorsky@gmail.com>
2018-12-06 21:38:55 -0500

    put in some gravity changes so things move around in clockwise fashion

--- js/shapes.js
+++ js/shapes.js
@@ -6,8 +6,9 @@
 // const Engine = Matter.Engine
 // const Render = Matter.Render
 // deconstructing the aliases so, e.g. const Engine = Matter.Engine
-const {Engine, Render, Bodies, World, MouseConstraint, Composites} = Matter
+const {Engine, Render, Bodies, World, MouseConstraint, Composites, Query} = Matter
 
+Matter.use('matter-wrap')
 
 // where is matter being deployed
 const sectionTag = document.querySelector("section.shapes")
@@ -39,11 +40,17 @@ const createShape = function(x,y) {
                 xScale: 0.5,
                 yScale: 0.5
             }
-        }
+        },
+        // plugin: {
+        //     wrap: {
+        //         min: {x: 0, y: 0},
+        //         max: {x: w, y: h}
+        //     }
+        // }
     })
 }
 
-const bigBall = Bodies.circle(w / 2, h / 2, 250, {
+const bigBall = Bodies.circle(w / 2, h / 2, Math.min(w/4, h/4), {
         isStatic: true,
         render: {
             fillStyle: "#ffffff"
@@ -91,10 +98,46 @@ World.add(engine.world, [
 // when we click, add a new shape
 document.addEventListener("click", function(event) {
     const shape = createShape(event.pageX, event.pageY)
+    initialShapes.bodies.push(shape)
     World.add(engine.world, shape)
 })
+
+// when we move our mouse, check matter for any collision
+// does the mouse touch a body
+
+// document.addEventListener("mousemove", function(event) {
+//     const vector = { x: event.pageX, y: event.pageY }
+//     const hoveredShapes = Query.point(initialShapes.bodies, vector)
+
+//     hoveredShapes.forEach(shape => {
+//         shape.render.sprite = null
+//         shape.render.fillStyle = "red"
+//     })
+
+// })
 
 // run both the engine and the renderer
 
 Engine.run(engine)
 Render.run(renderer)
+
+
+// mess with gravity
+
+let time = 0 
+
+const changeGravity = function () {
+    time = time + 0.02
+    engine.world.gravity.x = Math.sin(time)
+    engine.world.gravity.y = Math.cos(time)
+    requestAnimationFrame(changeGravity)
+}
+changeGravity()
+
+
+// // mess with gravity depending on mobile device orientation
+
+// window.addEventListener("deviceorientation", function(event) {
+//     engine.world.gravity.x = event.gamma / 30
+//     engine.world.gravity.y = event.beta / 30
+// })
