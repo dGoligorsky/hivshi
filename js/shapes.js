dee227311397d06a408f20c4e5e9ec676e333bf6
David Goligorsky <d.goligorsky@gmail.com>
2018-12-06 21:02:34 -0500

    made the shapes a rectangle with a texture fill and added the split using mix-blend-mode

--- js/shapes.js
+++ js/shapes.js
@@ -5,8 +5,8 @@
 // alias to make the code cleaner
 // const Engine = Matter.Engine
 // const Render = Matter.Render
-// deconstructing the aliases
-const {Engine, Render, Bodies, World} = Matter
+// deconstructing the aliases so, e.g. const Engine = Matter.Engine
+const {Engine, Render, Bodies, World, MouseConstraint, Composites} = Matter
 
 
 // where is matter being deployed
@@ -30,9 +30,15 @@ const renderer = Render.create({
 
 // have the ability to create a shape on the page
 const createShape = function(x,y) {
-    return Bodies.circle(x, y, 20 + 20 * Math.random(), {
+    return Bodies.rectangle(x, y, 38, 50, {
+        // frictionAir: 0.1,
         render: {
-            fillStyle: "red"
+            // fillStyle: "red", 
+            sprite: {
+                texture: "assets/outline-2x.png",
+                xScale: 0.5,
+                yScale: 0.5
+            }
         }
     })
 }
@@ -43,7 +49,44 @@ const bigBall = Bodies.circle(w / 2, h / 2, 250, {
             fillStyle: "#ffffff"
         }
 })
-World.add(engine.world, bigBall)
+
+const wallOptions = {
+    isStatic: true,
+    render: {
+        fillStyle: "#0033cc",
+        visible: true
+    }
+}
+
+const ground = Bodies.rectangle(w /2, h + 50, w + 100, 100, wallOptions)
+const ceiling = Bodies.rectangle(w /2, -50, w + 100, 100, wallOptions)
+const leftWall = Bodies.rectangle(-50, h / 2, 100, h + 100, wallOptions)
+const rightWall = Bodies.rectangle(w + 50, h / 2, 100, h + 100, wallOptions)
+
+// make the mouse interact with stuff
+const mouseControl = MouseConstraint.create(engine, {
+    element: sectionTag,
+    constraint: {
+        render: {
+            visible: false
+        }
+    }
+})
+
+const initialShapes = Composites.stack(50, 50, 20, 15, 40, 40, function(x, y) {
+    return createShape(x, y)
+})
+
+// add things to the world in an array
+World.add(engine.world, [
+    bigBall, 
+    ground,
+    ceiling,
+    leftWall,
+    rightWall,
+    mouseControl, 
+    initialShapes
+])
 
 // when we click, add a new shape
 document.addEventListener("click", function(event) {
